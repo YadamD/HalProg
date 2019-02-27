d0dea665b6c0a89b0a9447688a34155b8c48a5ae
YadamD <csabai.bence@gmail.com>
2019-02-27 14:37:48 +0100

    Add files via upload
    Gyokkereses

---
+++ fel2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+double sqrt_newton(double num, double x0){
+    double xnew=x0;
+    double delta=1;
+    int k=1;
+    while(delta>0.001 and k<21){
+        double xold=xnew;
+        xnew=xnew-(xnew*xnew-num)/(2*xnew);
+        delta=(xold-xnew)*(xold-xnew);
+        k++;
+
+    }
+    return xnew;
+
+double sqrt_newton_for(double num, double x0){
+    double xnew=x0;
+    for(int i=0; i<5; i++){
+        xnew=xnew-(xnew*xnew-num)/(2*xnew);
+    }
+    return xnew;
+
+}
+
+int main(int, char**) {
+    double root=sqrt_newton(612, 35.6);
+    std::cout<<root;
+}
