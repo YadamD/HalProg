8681019c16ae05642f366962741d15ccd8848b9e
YadamD <csabai.bence@gmail.com>
2019-02-27 14:16:15 +0100

    newtoncotes

---
+++ newt.c
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+double fun(double x){
+    	    return cos(x) * exp(-1*x*x);
+    	}
+
+double integrate(int n, double x0, double x1) {
+	double ans=0;
+	double x_0, x_1;
+	double h=(x1-x0)/n ;
+	for(int i=0; i<n+1; i++){
+    	x_0=x0+i*h;
+    	x_1=x_0+h;
+    	ans+=(fun(x_1)+fun(x_0))*h/2;
+	}
+	return ans;
+}
+
+int main() {
+	double result;
+	double x_0 = -1, x_1 = 3;
+	std::cout.precision(16);
+    for(int i = 0; i < 6; i++){
+	    result = integrate(pow(10,i+2), x_0, x_1);
+	    std::cout<<"n = "<<pow(10,i+2)<<'\n'<<result<<'\n';
+    }
+    std::cout << "Expected value: 1.346387956803450376698"<<'\n';
+  return 0;
+}
